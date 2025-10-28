9d534eee281a929cb344aa8fc6d168e39df7feda
SebastianKrupinski <krupinskis05@gmail.com>
2025-10-28 14:29:11 -0400

    Fix name space
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- lib/Responses/Tasks/TaskGet.php
+++ lib/Responses/Tasks/TaskGet.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2025 Sebastian Krupinski <krupinski01@gmail.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-namespace JmapClient\Responses\Calendar;
+namespace JmapClient\Responses\Tasks;
 
 use JmapClient\Responses\ResponseGet;
 
