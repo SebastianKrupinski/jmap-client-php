8afd7b4e809d1280ab202e71c87a8ce8b6d7d5d4
SebastianKrupinski <krupinskis05@gmail.com>
2026-05-02 15:50:04 -0400

    chore: update license
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- lib/Responses/Tasks/TaskListParameters.php
+++ lib/Responses/Tasks/TaskListParameters.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /**
  * SPDX-FileCopyrightText: 2025 Sebastian Krupinski <krupinski01@gmail.com>
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-only
  */
 
 namespace JmapClient\Responses\Tasks;
